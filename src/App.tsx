0610c7d5e8c59e9eff81de413c6bd9e571d235bf
unknown <harshildsheth@gmail.com>
2024-02-23 16:01:07 +0530

    porject created

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='Appbar flex flex-row'>
+        Name
+        <div>
+          Credit Card 
+        </div>
+        <div>
+          Password
+        </div>
+        <div>
+          Login
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
